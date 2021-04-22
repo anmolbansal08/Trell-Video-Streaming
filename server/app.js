78fb0bc1b23e1a50a204e276a7e2ad19456223a4
paras <parasmadan15@yahoo.com>
2021-04-22 16:06:16 +0530

    added video streaming

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const axios  = require('axios');
 
 app.get('/streams/:id', (req, res) => {
-    res.render('stream.ejs',{
+    res.render("stream.ejs",{
         id: req.params.id
-    })
+      });
+      console.log(req.params.id);
 })
 
 const port = process.env.PORT || 3000;
