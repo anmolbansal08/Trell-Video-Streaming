f786bb35089a36b2d0f94fa3f4a8d01d4b37722d
paras <parasmadan15@yahoo.com>
2021-04-22 19:57:14 +0530

    added stream stats

--- server/app.js
+++ server/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const axios  = require('axios');
 
+app.get('/streams/details/:id', async (req, res) => {
+    let result = await axios.get(`http://localhost:8000/api/streams/`)
+    console.log(result.data)
+    res.json(result.data.live[req.params.id]);
+})
+
 app.get('/streams/:id', (req, res) => {
     res.render("stream.ejs",{
         id: req.params.id
