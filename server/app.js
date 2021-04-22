da9111a4d04278d23c2b7081b7b91e8d62514c20
paras <parasmadan15@yahoo.com>
2021-04-22 14:56:45 +0530

    added video

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/streams/:id', (req, res) => {
-    res.render('stream.ejs')
+    res.render('stream.ejs',{
+        id: req.params.id
+    })
 })
 
 const port = process.env.PORT || 3000;
