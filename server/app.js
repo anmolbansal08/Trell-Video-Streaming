e88f0af511fbe9976740bcf736bd18949a281d36
paras <parasmadan15@yahoo.com>
2021-04-22 14:29:04 +0530

    ejs

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/streams/:id', (req, res) => {
-    res.send('Hello World!')
+    res.render('stream.ejs')
 })
 
 const port = process.env.PORT || 3000;
