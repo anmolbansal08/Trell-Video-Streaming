ffd132b2aeab1eba97398d2d137b8baabffe9e3a
paras <parasmadan15@yahoo.com>
2021-04-22 14:19:19 +0530

    created server

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/streams/:id', (req, res) => {
+    res.send('Hello World!')
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App Listening at http://localhost:${port}`)
+})
